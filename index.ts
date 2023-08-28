4e197c0eed9af76ad6b8e047c88a88b37f8f474c
NowWeAre-Free <kontodarm@wp.pl>
2023-08-28 22:25:45 +0200

    :tada: init

---
+++ index.ts
@@ -0,0 +1,9 @@
+const marcin = { age: 26, name: "Marcin", city: "Poznań" };
+
+const displayInfo = (person) => {
+  const age = person.age;
+  const name = person.name;
+  const city = person.city;
+};
+
+displayInfo(marcin);
