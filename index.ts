19de7e74e5ba85d8a5e7f7e8ea9bc3d5f5a19a0a
NowWeAre-Free <kontodarm@wp.pl>
2023-08-28 22:27:16 +0200

    way 1

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 const marcin = { age: 26, name: "Marcin", city: "Poznań" };
 
 const displayInfo = (person) => {
-  const age = person.age;
-  const name = person.name;
-  const city = person.city;
+  //   const age = person.age;
+  //   const name = person.name;
+  //   const city = person.city;
+
+  const { age, name, city } = person;
 };
 
 displayInfo(marcin);
